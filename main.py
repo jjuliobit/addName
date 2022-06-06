24dd9cebbb7cdd34a564dd767388a23d1282f9df
Julio S <66489452+jjuliobit@users.noreply.github.com>
2022-06-06 13:29:54 -0300

    Add files via upload
    Adicionar os nomes , excluir de um nome ou excluir tudo de uma vez , e ver uma lista que foram adicionados.

---
+++ main.py
@@ -0,0 +1,45 @@
+from ast import Index
+
+
+lista = []
+
+def add(a):
+    lista.append(a)
+    if lista[str] == lista[str]:
+        print('não pode ')
+    return
+
+
+def excluir(e):
+    lista.remove(e)
+    return
+
+def limpar(a):
+    lista.clear()
+    return a
+    
+
+def ver_lista(u):
+    if lista == []:
+        print('==========Lista vazias===========')
+    else:
+        print('==========Você está na lista==========')
+        print(lista)
+    return u 
+
+
+while True:
+ user = input('digite adicionar , apagar, apagar tudo ou lista: ')
+ if user == 'adicionar':
+     ad = input('qual nome que deseja adicionar? ')
+     add(ad)
+ elif user == 'apagar':
+      d = input('qual nome que deseja apagar, ou deseja tudo digite apagar tudo? ')
+      excluir(d)
+ elif user == 'apagar tudo':
+     cl = input('ceterza que quer apagar tudo?\n digite (s) ou (n): ')
+     limpar(cl)
+ elif user == 'lista':
+      ver_lista(user)
+   
+     
