56a4f658e785e16f83c07f002b34d853da0d9f17
Julio S <66489452+jjuliobit@users.noreply.github.com>
2022-06-20 09:38:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
-lista = []
+lista ={
+    'nome': [], 
+    'idade': [],
+}
 
 
 def add(a):
-    if ad in lista:
-        print('O nome já existe na lista.')
+    if ad_nome in lista['nome']:
+        print(f'O nome já existe na lista. ({ad_nome.upper()})')
+    else: 
+     print(f'Adicionado o nome: ({ad_nome.upper()}) na lista com sucesso.') 
+     lista['nome'].append(a)
+
+def add_idade(a):
+    if ad_idade in lista['idade']:
+     print(f'A idade já existe na lista. ({ad_idade})')
     else:
-        print(f'Adicionado: {ad} na lista com sucesso.')
-        lista.append(a)
+     print(f'Adicionado a idade: ({ad_idade}) na lista com sucesso.')
+     lista['idade'].append(a)
+
 
 def remover(a):
-    lista.remove(a)
-    return
+    if not lista in lista['nome']:
+     lista['nome'].remove(a)
+    return a
 
 def remover_tudo(a):
     lista.clear()
+    print()
+    print('Removido com sucesso...')
+    ver_lista()
     return a
 
 def ver_lista():
     if not lista == []:
+     print()
      print('========== LISTA ==========')
-     print(f'Listas: {lista}')
+     for nomes in lista.items():
+        print(nomes)
     else:
         print('========== LISTAS VAZIAS ==========')
+        print({lista})
 
 
 while True:
     user = input('Digite\n add, remove, remove all, lista: ')
     if user == 'add':
-        ad = input('Qual nome que deseja adicionar? ')
-        add(ad)
+        ad_nome = input('Qual é seu nome?\n -> ')
+        add(ad_nome)
+        ad_idade = input('Qual é sua idade?\n -> ')
+        add_idade(ad_idade)
     if user == 'remove':
-        remova = input('qual nome que deseja remover? ')
+        remova = input('Deseja remover o nome ou idade? ')
         remover(remova)
     if user == 'remove all':
-        remova_all = input('Certeza que quer remover tudo? Digite (s) ou (n)')
-        if remova_all == 's':
+        remova_all = input('Certeza que quer remover tudo? Digite\n (s) ou (n) -> ')
+        if remova_all != 's':
+          print('Não foi removidos.')
+        else:
             remover_tudo(remova_all)
     if user == 'lista':
         ver_lista()
